99e73c0d1de3d5dfd868b25306c1936d68f8cd92
Justin Bigishiro <justin.rvd@gmail.com>
2023-07-21 07:08:22 -0500

    complete lab

--- lib/teacher.py
+++ lib/teacher.py
@@ -4,7 +4,12 @@ from user import User
 
 import random
 
+# Assuming the User class is already defined in user.py
+
 class Teacher(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = ["Math", "Science", "History"]  # Example knowledge list
 
     def teach(self):
-        pass
+        return self.knowledge[0]  # Teaching the first item in the knowledge list
